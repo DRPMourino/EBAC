ef76fcceb9e1a18e5b25e3bd4dc391960b8bd91f
DRPMourino <danrob992010@hotmail.com>
2024-01-15 18:09:54 -0300

    ebac

---
+++ JAVA FULL STACK/JAVA/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+
+    public static void main(String [] args) {
+        String ambiente = args[0];
+        if (ambiente.equalsIgnoreCase("DEV")) {
+        System.out.print("Execuntando em DEV");
+        } else if (ambiente.equalsIgnoreCase("TEST")) {
+            System.out.print("Execuntando em TEST");
+        } else  {
+            System.out.print(ambiente);
+        }
+
+    }
+
+}
